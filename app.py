3f62fbe3bd2c4f4f9425592d60675c5e9b4b726e
WIKTOR WIJATKOWSKI <victor.wijatkowski@gmail.com>
2021-10-11 14:02:22 +0200

    Added Bootrstrap and view contruction

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="0.0.0.0", debug=True)
