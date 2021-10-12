3aa569099d74a6b12e97cd4da7bae0340a383def
WIKTOR WIJATKOWSKI <victor.wijatkowski@gmail.com>
2021-10-12 14:58:30 +0200

    BookList Added and create request for this list.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/Data_base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
@@ -13,8 +15,17 @@ db = SQLAlchemy(app)
 def home():
     return render_template('home.html')
 
-@app.route('/books-list'):
+
+@app.route('/books-list')
 def books_list():
+    response = requests.get('https://www.googleapis.com/books/v1/volumes?q=Hobbit')
+
+    if response.status_code == 200:
+        books_founded = response.json()
+        books = books_founded['items']
+
+        return render_template('bookslist.html', books=books)
+
 
 
 
