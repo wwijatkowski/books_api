9426846359b53b717f3d44914c99c435d294bdbf
WIKTOR WIJATKOWSKI <victor.wijatkowski@gmail.com>
2021-10-13 13:20:45 +0200

    Added bookdetails route and template

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 import requests
+from jinja2 import UndefinedError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/Data_base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-
 
 
 @app.route('/')
@@ -18,7 +18,7 @@ def home():
 
 @app.route('/books-list')
 def books_list():
-    response = requests.get('https://www.googleapis.com/books/v1/volumes?q=Hobbit')
+    response = requests.get('https://www.googleapis.com/books/v1/volumes?q=Hobbit', )
 
     if response.status_code == 200:
         books_founded = response.json()
@@ -27,6 +27,22 @@ def books_list():
         return render_template('bookslist.html', books=books)
 
 
+@app.route('/book-details')
+def book_details():
+    book_id = request.args.get('book_id')
+
+    response = requests.get(f'https://www.googleapis.com/books/v1/volumes/{book_id}')
+
+    if response.status_code == 200:
+        book = response.json()
+
+        # This handling an exception is because sometimes there is no imageLinks - not exist
+        try:
+            thumbnail = book['volumeInfo']['imageLinks']['thumbnail']
+        except KeyError:
+            thumbnail = ''
+
+        return render_template('bookdetails.html', book=book, thumbnail=thumbnail)
 
 
 if __name__ == '__main__':
